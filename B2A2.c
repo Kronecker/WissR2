bc50759adc686c3004b81cff13c58730885d045f
Kronecker <benjamin.grabiger@uni-jena.de>
2019-05-12 21:21:30 +0200

    Start Blatt2 Aufgabe 2a

---
+++ B2A2.c
@@ -0,0 +1,107 @@
+//
+// Created by Looky on 12/05/2019.
+//
+#include <mpi.h>
+#include <stdio.h>
+
+
+
+#define N 500000
+
+
+double* allocInitVecAUp(int startIndex, int numElements);
+double* allocInitVecBDwn(int startIndex, int numElements);
+
+
+
+
+
+
+
+
+double* allocInitVecAUp(int startIndex, int numElements) {
+    double* vec;
+    vec = (double*) malloc(sizeof(double) * numElements)
+    for (int k=0; k<numElements; k++) {
+        vec[k]=k+1+startIndex;
+    }
+    return vec;
+}
+
+
+double* allocInitVecBDwn(int startIndex, int numElements) {
+    double* vec;
+    vec = (double*) malloc(sizeof(double) * numElements)
+    for (int k=0; k<numElements; k++) {
+        vec[k]=N-k-startIndex;
+    }
+    return vec;
+}
+
+
+
+int main (int argc,char *argv[]) {
+    printf("Hello world");
+    int rank, size;
+    MPI_INIT(&argc, char &argv[]);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    double *vecA, *vecB;
+
+    vecA=allocInitVecAUp(10,2);
+    vecB=allocInitVecBDwn(10,2);
+    // Compute Scalprod
+
+
+    // Communicate with master and send results
+    if(rank==0) {
+        // rcv
+
+    }  else  {
+        // send
+
+    }
+
+
+
+
+
+    free(vecA);
+    free(vecB);
+
+
+    MPI_Finalize();
+    printf("Hello world");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- makefile
+++ makefile
@@ -3,4 +3,4 @@ numProc=4
 B2A1: B2A1.c
 	mpicc B2A1.c -o B2A1.out
 B2A1run: B2A1
-	mpirun $(numProc) B2A1.out
+	mpirun -n $(numProc) B2A1.out
