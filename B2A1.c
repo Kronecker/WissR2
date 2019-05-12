1fd624f0187086c305e9fe08afc49bbf4d0eba94
Kronecker <benjamin.grabiger@uni-jena.de>
2019-05-12 19:58:54 +0200

    git init

--- B2A1.c
+++ B2A1.c
@@ -1,6 +1,20 @@
-#include <iostream>
+#include <stdio.h>
+#include <mpi.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+main(int argc,char *argv[]){
+
+    int rank,size;
+    MPI_Init(&argc,&argv); // MPI starten
+    MPI_Comm_size(MPI_COMM_WORLD,&size); // Anzahl der Prozesse ermitteln
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank); // Nummer des aktuellen Prozesse
+
+    if(rank==0){
+
+        printf("Hello World from Master of %d Processes.\n",size);
+
+    }else{
+
+        printf("Hello World from Slave %d.\n",rank);
+    }
+    MPI_Finalize(); // MPI beenden
 }
---
+++ makefile
@@ -0,0 +1,2 @@
+B2A1: B2A1.c
+    mpicc B2A1.c -o B2A1.out
