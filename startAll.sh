143a6a2f0007635db4e0d0b8cb9c4aee16a0952e
Kronecker <benjamin.grabiger@uni-jena.de>
2019-05-18 20:00:46 +0200

    Start Script for time measure

--- startAll.sh
+++ startAll.sh
@@ -2,17 +2,18 @@
 echo "Hi"
 numProcs=16
 runs=10
-rm logfile.log
-touch logfile.log
+log=logfile.log
+rm ${log}
+touch ${log}
 
 for fileRun in "B2A2_SNDRCV.out" "B2A2_REDUCE.out" "B2A2_SNDRCV_Tree2Nodes.out" "B2A2_SNDRCV_Tree16Nodes.out"
 do
-
+echo ${fileRun}
 for numProcs in 1 2 4 6 8 16 24 32 48 64
 do
-for i in {1..runs}
+for i in {1..${runs}}
 do
-    mpirun -n $numProcs $fileRun >> logfile.log
+    mpirun -n ${numProcs} ${fileRun} >> ${log}
 done
 
 done
