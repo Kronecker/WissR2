307793ad7013b8fd7ffe35bb02aea60a146a5c0f
Kronecker <benjamin.grabiger@uni-jena.de>
2019-06-24 14:01:49 +0200

    Blatt 5

---
+++ B5A10.cpp
@@ -0,0 +1,79 @@
+//
+// Created by looky on 24.06.2019.
+//
+
+
+#include <iostream>
+#include <math.h>
+#include <fstream>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- makefile
+++ makefile
@@ -1,10 +1,8 @@
 CFLAGS=
 COMPILER=g++
-all: B4A8
+all: B5A10
 
 clean:
 	rm *.out
-B4A8: 	B4A8.cpp
-	$(COMPILER) B4A8.cpp $(CFLAGS) -o B4.out
-B4A8Debug: 	B4A8.cpp
-	$(COMPILER) B4A8.cpp -ggdb -o B4.out
+B5A10: B5A10.cpp
+	$(COMPILER) B5A10.cpp $(CFLAGS) -o B5_altSchwarz.out
